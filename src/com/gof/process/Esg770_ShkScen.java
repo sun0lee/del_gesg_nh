83df9f64ec77b8a9b7497ef7bfb477d6f3d1a0df
sun0lee <sun0lee88@gmail.com>
2023-06-26 21:24:58 +0900

    770 : ltfrA 수정
    자산할인율에서 부채의 ltfr : 시장에서 관찰되는 최장만기 금리를 그대로 사용
    (기준서 : 국고채 50년물까지의 금리를 Smith-Wilson 방식으로 보간한 수익률 곡선을 사용)
    
    시나리오 별 충격 스프레드 반영 방안
    - > 시나리오별 관측만기에 해당하는 스프레드 값 반영함.
    - > TODO : 스프레드 산출하는 tenor와 시장 관측 최장만기과 불일치로 현재는 50년물, 30년물 스프레드가 없음 !! (지금은 있는 값 중 max로 하드코딩 / 코드 오류 확인 목적 )
    
    ** check : 부채의 경우 ltfr을 시나리오와 관계없이 동일한 값을 사용 (장기선도금리 근방으로 수렴하는 형태임)

--- src/com/gof/process/Esg770_ShkScen.java
+++ src/com/gof/process/Esg770_ShkScen.java
@@ -6,8 +6,10 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.gof.enums.EJob;
+import com.gof.dao.IrCurveSpotDao;
 import com.gof.dao.IrCurveYtmDao;
 import com.gof.dao.IrDcntRateDao;
 import com.gof.dao.IrSprdDao;
@@ -16,6 +18,7 @@ import com.gof.entity.IrCurveYtm;
 import com.gof.entity.IrDcntRateBuIm;
 import com.gof.entity.IrDcntSceIm;
 import com.gof.entity.IrParamSw;
+import com.gof.entity.IrSprdAfnsCalc;
 import com.gof.model.SmithWilsonKics;
 import com.gof.model.entity.SmithWilsonRslt;
 import com.gof.util.DateUtil;
@@ -47,7 +50,9 @@ public class Esg770_ShkScen extends Process {
 
 			// 자산 sw 보간 목적 ltfr
 			List<IrCurveYtm> ytmList = IrCurveYtmDao.getIrCurveYtm(bssd, curveSwMap.getKey());
+			List<IrCurveSpot> spotList = IrCurveSpotDao.getIrCurveSpot(bssd, curveSwMap.getKey());
 
+			
 
 			if (swSce != null) {
 				
@@ -58,6 +63,9 @@ public class Esg770_ShkScen extends Process {
 	
 			for (int sceNo = 1; sceNo <= scenCnt; sceNo++) {
 			
+			Map<String, Double> irSprdShkMap = IrSprdDao.getIrSprdAfnsCalcList(bssd, irModelId, curveSwMap.getKey(), sceNo).stream()
+					.collect(Collectors.toMap(IrSprdAfnsCalc::getMatCd, IrSprdAfnsCalc::getShkSprdCont));				
+	
 			List<IrCurveSpot> irDcntRateBuImList = IrDcntRateDao.getIrDcntRateBuImToAdjSpotList(bssd, applBizDv, irModelId, curveSwMap.getKey(), sceNo);
 			if(irDcntRateBuImList.size()==0) {
 				log.warn("No IR Dcnt Rate Data [BIZ: {}, IR_CURVE_ID: {}, IR_CURVE_SCE_NO: {}] in [{}] for [{}]", applBizDv, curveSwMap.getKey(),sceNo , toPhysicalName(IrDcntRateBuIm.class.getSimpleName()), bssd);
@@ -76,10 +84,19 @@ public class Esg770_ShkScen extends Process {
 				String matCd = ytmList.get(ytmList.size() - 1).getMatCd();
 				int matMonths = Integer.parseInt(matCd.substring(1));
 				
-				// 23.06.26 좀 더 고민 : 그대로 사용해도 될지(ytm) => spot으로 전환한 값을 사용할지, spot도 연속으로 변환할지 이산형을 사용할지 ? 
-				double ltfrA = ytmList.get(ytmList.size() - 1).getYtm(); 
-				int ltfrTA = matMonths / 12; 
-				if (sceNo == 1) log.info("irCurveId :{}, ltfrA :{}, ltfrTA:{}", curveSwMap.getKey(), ltfrA, ltfrTA);
+				// 23.06.26 : ytm vs. spot? 
+				double   ytm    = ytmList.get(ytmList.size() - 1).getYtm(); 
+				int      ltfrTA = matMonths / 12; 
+				double   spot   = spotList.get(ytmList.size() - 1).getSpotRate();   // disc.
+				
+				// TODO : 임시 코드확인용/ spread를 시장에서 관측되는 데이터의 최장만기 기준으로 만들어야 하는데 지금은 없음. 
+				String tmpMatCd = curveSwMap.getKey().equals("1010000") ? "M0240" : "M0360";
+				
+				double spread = irSprdShkMap.get(tmpMatCd); // TODO tmpMatCd -> matCd
+			    double ltfrA = irDiscToCont(spot) + spread ; // TODO check !! 이렇게 정의하면 시나리오마다 spread값이 달라지므로 ltfrA값이 시나리오번호마다 달라짐. 
+			    
+//				if (sceNo == 1) log.info("irCurveId :{}, ltfrA :{}, ltfrTA:{}, ytm :{} , spot :{}, spread:{} ", curveSwMap.getKey(), ltfrA, ltfrTA, ytm, spot, spread);
+				log.info("sceNo :{}, irCurveId :{}, ltfrA :{}, ltfrTA:{}, ytm :{} , spot :{}, spread:{} ",sceNo,  curveSwMap.getKey(), ltfrA, ltfrTA, ytm, spot, spread);
 
 				
 				sw = new SmithWilsonKics(baseDate, irDcntRateBuImList, CMPD_MTD_CONT, true, ltfrA, ltfrTA, projectionYear, 1, 100, DCB_MON_DIF);
